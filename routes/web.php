af1a01881ab9796318c402477c4ef43af9be2cb0
parfenovichs <CoKoJl1k>
2021-08-20 11:45:48 +0300

    20.21.2021 - 2

--- routes/web.php
+++ routes/web.php
@@ -24,38 +24,29 @@ use App\Models\Posts;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
+
 
 Route::get('/', [MainController::class, 'index'])->name('welcome');
 
 Route::get('create', [CreateController::class, 'index'])->name('form');
 Route::post('create', [CreateController::class, 'create'])->name('create_form');
-
 Route::get('edit/{id}', [EditController::class, 'index'])->name('edit_form_get');
 Route::put('edit/{id}', [EditController::class, 'update'] )->name('edit_form_put');
-
 Route::delete('delete/{id}',[EditController::class, 'destroy'])->name('delete');
 
 
 Route::get('search',[SearchController::class, 'index'])->name('search');
 
-
 Route::get('errors_update', function () {
-
     return view('errors_update');
 })->name('errors_update');
 
 // routs test
-
 Route::get('list_of_posts', [ListOfPostsController::class, 'index'])->name('list_of_posts');
 
 
 Route::get('test2', function () {
-    $obj = new TestController();
+    $obj =  new TestController;
     return $obj->index();
 })->name('test2');
 
